e2b2ccae74cab9633f7a75f1f8b622dd2a6f2a6e
Maitret17 <tombieleg@gmail.com>
2026-09-11 10:09:41 +0200

    Initial Spring Boot project

---
+++ src/main/java/fr/efrei/brackethub/BrackethubApplication.java
@@ -0,0 +1,13 @@
+package fr.efrei.brackethub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrackethubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrackethubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/efrei/brackethub/BrackethubApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.efrei.brackethub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrackethubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
