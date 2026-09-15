fe2cc7d0d61ff160da56f0a0f19ee8cb99e2a05c
Maitret17 <tombieleg@gmail.com>
2026-09-15 14:59:58 +0200

    Add tournament entity, repository, service, and controller

--- src/main/java/fr/efrei/brackethub/service/TournamentService.java
+++ src/main/java/fr/efrei/brackethub/service/TournamentService.java
@@ -1,4 +1,38 @@
 package fr.efrei.brackethub.service;
 
+import fr.efrei.brackethub.data.Tournament;
+import fr.efrei.brackethub.repository.TournamentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
 public class TournamentService {
+
+    @Autowired
+    private TournamentRepository tournamentRepository;
+
+    public TournamentService() {
+    }
+
+    public List<Tournament> listOfTournaments() {
+        return tournamentRepository.findAll();
+    }
+
+    public void addTournament(Tournament tournament) {
+        tournamentRepository.save(tournament);
+    }
+
+    public Tournament getTournament(Long id) {
+
+        return tournamentRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "This tournament wasn't found"
+                ));
+    }
 }
--- src/main/java/fr/efrei/brackethub/web/TournamentController.java
+++ src/main/java/fr/efrei/brackethub/web/TournamentController.java
@@ -1,4 +1,39 @@
 package fr.efrei.brackethub.web;
 
+import fr.efrei.brackethub.data.Tournament;
+import fr.efrei.brackethub.service.TournamentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tournaments")
 public class TournamentController {
+
+    @Autowired
+    private TournamentService tournamentService;
+
+    public TournamentController() {
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Tournament> listOfTournaments() {
+        return tournamentService.listOfTournaments();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Tournament getTournament(@PathVariable Long id) {
+        return tournamentService.getTournament(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addTournament(@RequestBody Tournament tournament) {
+        tournamentService.addTournament(tournament);
+    }
 }
